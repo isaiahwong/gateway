881ac868238c712868c083a773bc2dd7131fe3da
isaiahwong <isaiah@jirehsoho.com>
2019-05-12 01:40:02 +0800

    Added new whitelists

--- gateway/src/middlewares/cors.js
+++ gateway/src/middlewares/cors.js
@@ -3,14 +3,14 @@ import { NotAuthorized } from '../libs/errors';
 
 const whitelist = [
   process.env.DOMAIN, 
+  process.env.FRONT_END,
   process.env.DEV_CLIENT,
   `http://localhost:${process.env.PORT}`,
 ];
 
 const corsOptions = {
   origin: (origin, cb) => {
-    console.log(origin)
-    if (whitelist.indexOf(origin) !== -1 || (!origin)) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       cb(null, true);
     } 
     else {
